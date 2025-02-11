caab7ec6f811daf6bc88b30629819009e8abfb56
Robert Platt <rp1818@ic.ac.uk>
2025-02-11 14:10:56 +0000

    fix: flake8 compliance

--- helper.py
+++ helper.py
@@ -6,7 +6,9 @@ import numpy as np
 from main import RATIO_DATA
 import onnxruntime
 
-ort_session = onnxruntime.InferenceSession("1024_test.onnx", providers=["CPUExecutionProvider"])
+ort_session = onnxruntime.InferenceSession(
+    "1024_test.onnx", providers=["CPUExecutionProvider"]
+)
 
 
 def get_pixel_data(file_path):
@@ -14,17 +16,20 @@ def get_pixel_data(file_path):
     image = CRISMImage(file_path)
     image.ratio_image(RATIO_DATA)
     ratioed_im = image.ratioed_image
-    ratioed_im = ratioed_im.reshape(-1, 438) # 438 bands
+    ratioed_im = ratioed_im.reshape(-1, 438)  # 438 bands
     ratioed_im, _ = clip_bands(ratioed_im)
-    ratioed_im = ratioed_im[:, :248] # 248 bands to use for the model
+    ratioed_im = ratioed_im[:, :248]  # 248 bands to use for the model
     ratioed_im, _ = impute_bad_values_in_image(ratioed_im, threshold=10)
-    min_vals = np.min(ratioed_im, axis = -1, keepdims=True)
-    max_vals = np.max(ratioed_im, axis = -1, keepdims=True)
-    ratioed_im_scaled = (ratioed_im - min_vals) / ((max_vals - min_vals)+0.00001)
+    min_vals = np.min(ratioed_im, axis=-1, keepdims=True)
+    max_vals = np.max(ratioed_im, axis=-1, keepdims=True)
+    ratioed_im_scaled = (ratioed_im - min_vals) / (
+        (max_vals - min_vals) + 0.00001
+    )
 
     return ratioed_im_scaled
 
+
 def predict(data):
-    input_dictionary = {'input.1': np.float32(data)}
+    input_dictionary = {"input.1": np.float32(data)}
     runtime_outputs = ort_session.run(None, input_dictionary)
-    return runtime_outputs 
+    return runtime_outputs
